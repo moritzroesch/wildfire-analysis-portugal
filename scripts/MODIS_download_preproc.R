518afaa2652964112a72548e6fbc16af2c6b6f88
Moritz Rösch <moritz.roesch@gmx.de>
2022-03-14 15:38:35 +0100

    successful run of MODIS download an pre-processing

--- scripts/MODIS_download_preproc.R
+++ scripts/MODIS_download_preproc.R
@@ -2,13 +2,19 @@
 ##
 ## Script name: MODIS_download_preproc.R
 ##
-## Purpose of script: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+## Purpose of script: Downloading and precprocessing of MOD09A1 8-day SR product
 ##
-## Sections:  XXXXX
-##              - XXXXX
+## Sections:  Get country vector data
+##              - download, filter and reproject vector data for Portugal
 ##
-##            XXXX
-##              - XXXXX
+##            MODIStsp download and processing
+##              - define pre-required variables for MODIStsp function
+##              - query NASA earthdata credentials
+##              - automated download and pre-processing for five selected MODIS scenes
+##
+##            MODIS data stacking
+##              - pulling of data from MODIStsp folder structures
+##              - creation of raster stacks
 ##
 ## Author: Moritz Rösch
 ##
@@ -17,9 +23,11 @@
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 
 
+
 # Packages ----------------------------------------------------------------
 
 library(sf)
+library(terra)
 library(tidyverse)
 library(MODIStsp) # tools for downloading and preprocessing MODIS data
 # install.packages("MODIStsp") for any problems with installation on LINUX systems
@@ -46,15 +54,15 @@ prt <- prt %>%
 # available (see https://cran.r-project.org/web/packages/MODIStsp/vignettes/MODIStsp.html).
 # With help of the MODIStsp GUI a .json file (opts_file) was created defining the product
 # and processing parameters. Following processing steps are automatically applied
-# to the selected MODIS product (for all details see "auxiliary/MOD009A1_PRT_setup.json"):
-#   - Selection of MOD09A1 8 days surface reflection product (500m)
+# to the selected MODIS product (for all details see "auxiliary/MOD09A1_PRT_setup.json"):
+#   - Selection of MOD09A1 8-days surface reflection product (500m)
 #   - subset to B1 (Red), B2 (NIR), B3 (Blue), B4 (Green), B6 (SWIR)
 #   - mosaicing of Terra MODIS scenes to cover the whole AOI of Portugal
 #   - Clip to bounding box of prt.gpkg
 #   - reprojection to local WGS 84 / UTM zone 29N (EPSG:32629)
 
 opts_file <- "auxiliary/MOD09A1_PRT_setup.json" # MODIStsp setup file with pre-defined processing parameters
-acq_dates <- c("2021.10.08", "2020.10.07") # MODIS Terra acquisition dates
+acq_dates <- c("2017.10.08", "2018.10.08", "2019.10.08", "2020.10.07","2021.10.08")  # MODIS Terra acquisition dates
 out_folder <- str_c(getwd(),"/data") # full path to output folder for processed MODIS products and raw data 
 
 # Run function to enter your NASA Earthdata login credentials:
@@ -86,4 +94,28 @@ for (i in 1:length(acq_dates)){
            out_folder_mod = out_folder_year_raw,
            parallel = TRUE)
   print(str_c("MOD09A1 download and processing for ", acq_date, " done."))
+}
+
+
+
+# MODIS data stacking -----------------------------------------------------
+
+years <- as.character(c(2017:2021))
+MODIS_stacks <- list()
+for (i in years){
+  
+  # move into directory of year
+  out_folder_bands <- str_c(out_folder, "/", i, "/Surf_Ref_8Days_500m_v6")
+  bands_list <- list()
+  for (j in list.files(out_folder_bands)){
+    path_to_band <- str_c(out_folder_bands, "/", j)
+    r <- rast(list.files(path_to_band, full.names = TRUE)) # load single MODIS band
+    bands_list[j] <- r
+  }
+  r_stack <- rast(bands_list) #stack all MODIS bands of one scene
+  MODIS_stacks[i] <- r_stack
+  writeRaster(r_stack, str_c(out_folder, "/", i, "/MOD09A1_", 
+                             str_subset(acq_dates, pattern = i),
+                             "_stack.tif"),
+              overwrite = TRUE)
 }

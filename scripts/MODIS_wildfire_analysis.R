5836acdde982870a36f04352b640f80556e11ce3
Moritz Rösch <moritz.roesch@gmx.de>
2022-03-15 13:51:00 +0100

    started wildfire script and initialized NBR function

--- scripts/MODIS_wildfire_analysis.R
+++ scripts/MODIS_wildfire_analysis.R
@@ -2,17 +2,45 @@
 ##
 ## Script name: MODIS_wildfire_analysis.R
 ##
-## Purpose of script: Burnt area analysis of fire seasons 2017-2021 in
-##                    Victoria, Australia
+## Purpose of script: Burnt area analysis of fire seasons 2017-2021 in Protugal
 ##
-## Sections:  Pre-Processing
-##              - pre-processing of MODIS data
+## Sections:  Load data stacks
+##              - loading of pre-processed MODIS data stacks
 ##
-##            NBR calculation
-##              - calculation of NBR
+##            Normalized Burn Ratio (NBR) calculation
+##              - calculate NBR for yearly MODIS data
 ##
 ## Author: Moritz Rösch
 ##
-## Date: 2022-03-14
+## Date: 2022-03-15
 ##
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+
+
+
+# Packages ----------------------------------------------------------------
+
+library(terra)
+library(tidyverse)
+
+
+
+# Load data stacks --------------------------------------------------------
+
+years <- as.character(c(2017:2021))
+data <- list()
+for (i in years){
+  path_to_stack <- list.files(str_c("data/", i),
+                              pattern = "MOD09A1_\\d{4}\\.\\d{2}\\.\\d{2}_stack.tif",
+                              full.names = TRUE)
+  data[str_c("MOD09A1_", i)] <- rast(path_to_stack) # load and store MOD09A1 stack in list
+}
+data
+
+
+
+# NBR calculation ---------------------------------------------------------
+
+
+
+
--- scripts/utils/funcs.R
+++ scripts/utils/funcs.R
@@ -1,10 +1,51 @@
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+##
+## Script name: funcs.R
+##
+## Purpose of script: Outsourced functions for wildfire analysis project
+##
+## Funtions:  earthdata_credentials
+##              - queries user credentials for NASA Earthdata account
+##
+##            NBR
+##              - calculates Normalized Burn Ratio
+##
+## Author: Moritz Rösch
+##
+## Date: 2022-03-14
+##
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+
+
 
 # earthdata_credentials ---------------------------------------------------
-# Query user credentials for a NASA Earthdata account.
 
 earthdata_credentials <- function(){
+  # Query user credentials for a NASA Earthdata account.
+  
   credentials <<- list()
   credentials["user"] <<- readline(prompt = "Enter NASA Earthdata username/e-mail: ")
   credentials["password"] <<- readline(prompt = "Enter NASA Earthdata password: ")
+}
+
+
+
+# NBR ---------------------------------------------------------------------
+
+NBR <- function(r, NIR, SWIR, write_ras = FALSE, output_filename = NULL){
+  # Calculates Normalized Burn Ratio (NBR) of a raster stack based on defined
+  # band numbers for NIR and SWIR bands.
+  
+  # r:                SpatRaster or RasterBrick,
+  #                   Raster stack with at least two bands (e.g., NIR and SWIR).
+  # NIR:              numeric,
+  #                   NIR-band number.
+  # SWIR:             numeric,
+  #                   SWIR-band number.
+  # write_raster:     bool, default = FALSE,
+  #                   If TRUE, writeRaster function writes raster to directory of
+  #                   output_filename.
+  # output_filename:  string
+  #                   Define path and filename to write output raster layer.
 }
 

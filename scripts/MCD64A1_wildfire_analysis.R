2472809c66d0f22b98cb8ae82d56a0b5701bbcaa
Moritz Rösch <moritz.roesch@gmx.de>
2022-03-16 11:35:22 +0100

    changed data reading

--- scripts/MCD64A1_wildfire_analysis.R
+++ scripts/MCD64A1_wildfire_analysis.R
@@ -5,14 +5,12 @@
 ## Purpose of script: Burnt area analysis of fire seasons 2017-2021 in Protugal
 ##
 ## Sections:  Load data stacks
-##              - loading of pre-processed MODIS data stacks
+##              - loading of pre-processed MODIS timeseries stacks
 ##
-##            Normalized Burn Ratio (NBR) calculation
-##              - calculate NBR for yearly MODIS data
 ##
 ## Author: Moritz Rösch
 ##
-## Date: 2022-03-15
+## Date: 2022-03-16
 ##
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 
@@ -30,10 +28,13 @@ library(tidyverse)
 years <- as.character(c(2017:2021))
 data <- list()
 for (i in years){
-  path_to_stack <- list.files(str_c("data/", i),
-                              pattern = "MOD09A1_\\d{4}\\.\\d{2}\\.\\d{2}_stack.tif",
+  path_to_stack <- list.files(str_c("data", i, sep = "/"),
+                              pattern = "burnday_\\d{4}.tif",
                               full.names = TRUE)
-  data[str_c("MOD09A1_", i)] <- rast(path_to_stack) # load and store MOD09A1 stack in list
+  r <- rast(path_to_stack)
+  names(r) <- c("06_June", "07_July", "08_August", "09_September") # rename bands to months
+  data[str_c("burnday_", i)] <- r # store burnday stacks in data list for further analyzation
+  rm(r)
 }
 data
 

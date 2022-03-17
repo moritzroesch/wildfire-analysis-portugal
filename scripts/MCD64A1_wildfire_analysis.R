35efb355ec04deeb2c2f45293321b50272a823b3
Moritz Rösch <moritz.roesch@gmx.de>
2022-03-17 11:57:02 +0100

    conversion to polygons and filtering

--- scripts/MCD64A1_wildfire_analysis.R
+++ scripts/MCD64A1_wildfire_analysis.R
@@ -1,16 +1,20 @@
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 ##
-## Script name: MODIS_wildfire_analysis.R
+## Script name: MCD64A1_wildfire_analysis.R
 ##
-## Purpose of script: Burnt area analysis of fire seasons 2017-2021 in Protugal
+## Purpose of script: Burnt area analysis of fire seasons 2017-2021 in Portugal
 ##
 ## Sections:  Load data stacks
-##              - loading of pre-processed MODIS timeseries stacks
+##              - loading of pre-processed MODIS time series stacks
+##
+##            Raster time series to sf
+##              - convert yearly MCD64A1 stacks to sf object
+##              
 ##
 ##
 ## Author: Moritz Rösch
 ##
-## Date: 2022-03-16
+## Date: 2022-03-17
 ##
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 
@@ -19,6 +23,7 @@
 # Packages ----------------------------------------------------------------
 
 library(terra)
+library(sf)
 library(tidyverse)
 
 
@@ -32,8 +37,25 @@ for (i in years){
                               pattern = "burnday_\\d{4}.tif",
                               full.names = TRUE)
   r <- rast(path_to_stack)
-  names(r) <- c("06_June", "07_July", "08_August", "09_September") # rename bands to months
+  names(r) <- c("June_06", "July_07", "August_08", "September_09") # rename bands to months
   data[str_c("burnday_", i)] <- r # store burnday stacks in data list for further analyzation
   rm(r)
 }
 data
+
+
+
+# Raster time series to sf -------------------------------------------------
+
+for (i in 1:length(data)){
+  r <- data[[i]]
+  burn_poly <- r %>% 
+    as.polygons() %>% # polygonize each layer of stack
+    st_as_sf() %>% # convert to sf object
+    filter(!across(1, ~.==-2)) # removes rows with unburned (0), fill (-1) and water (-2) polygons in first column
+  burn_poly
+  
+  
+}
+
+st_write(spatvec, "data/spatvec_undis.gpkg")

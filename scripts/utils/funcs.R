9e64e76fa2b7fcaefc7a9868b7f22462fb4b84d4
Moritz Rösch <moritz.roesch@gmx.de>
2022-03-21 08:30:02 +0100

    added error messages to credentials query

--- scripts/utils/funcs.R
+++ scripts/utils/funcs.R
@@ -4,11 +4,8 @@
 ##
 ## Purpose of script: Outsourced functions for wildfire analysis project
 ##
-## Funtions:  earthdata_credentials
+## Functions:  earthdata_credentials
 ##              - queries user credentials for NASA Earthdata account
-##
-##            NBR
-##              - calculates Normalized Burn Ratio
 ##
 ## Author: Moritz Rösch
 ##
@@ -26,26 +23,11 @@ earthdata_credentials <- function(){
   credentials <<- list()
   credentials["user"] <<- readline(prompt = "Enter NASA Earthdata username/e-mail: ")
   credentials["password"] <<- readline(prompt = "Enter NASA Earthdata password: ")
+  if (credentials["user"] == "" & credentials["password"] == ""){
+    stop("Username and password missing.")
+  } else if (credentials["password"] == ""){
+    stop("Password missing.")
+  } else if (credentials["user"] == ""){
+    stop("Username missing.")
+  }
 }
-
-
-
-# NBR ---------------------------------------------------------------------
-
-NBR <- function(r, NIR, SWIR, write_ras = FALSE, output_filename = NULL){
-  # Calculates Normalized Burn Ratio (NBR) of a raster stack based on defined
-  # band numbers for NIR and SWIR bands.
-  
-  # r:                SpatRaster or RasterBrick,
-  #                   Raster stack with at least two bands (e.g., NIR and SWIR).
-  # NIR:              numeric,
-  #                   NIR-band number.
-  # SWIR:             numeric,
-  #                   SWIR-band number.
-  # write_raster:     bool, default = FALSE,
-  #                   If TRUE, writeRaster function writes raster to directory of
-  #                   output_filename.
-  # output_filename:  string
-  #                   Define path and filename to write output raster layer.
-}
-
